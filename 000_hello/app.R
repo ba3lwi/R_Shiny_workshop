f21375d8fee9973e76132d877dd36085d67bc0eb
Wail Ba alawi <wail.baalawi.business@gmail.com>
2022-03-12 04:08:00 -0500

    Added files

---
+++ 000_hello/app.R
@@ -0,0 +1,11 @@
+
+# Load package shiny
+library(shiny)
+# Define UI for application
+ui <- fluidPage(
+)
+# Define server logic
+server <- function(input, output) {
+}
+# Build and run the application
+shinyApp(ui = ui, server = server)
